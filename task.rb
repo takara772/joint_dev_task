6d34f95dd1ff0b9a81b1b44645a07f31c0b76a00
takara <toto170905@ymobile.ne.jp>
2020-05-31 20:52:04 +0900

    質問１３に回答

--- task.rb
+++ task.rb
@@ -135,7 +135,7 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-  user_data = user_data.merge!(update_data)
+  user_data.merge!(update_data)
     p user_data
   p user_data.update(update_data)
 end
